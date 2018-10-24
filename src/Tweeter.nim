7d6029a06aeae5c939a24d2e6d45324b3a68446a
Jamie Atkinson <jabbslad@gmail.com>
2018-10-24 23:29:57 +0100

    can now render homepage

--- src/Tweeter.nim
+++ src/Tweeter.nim
@@ -5,11 +5,27 @@ import asyncdispatch,
   views/user, 
   views/general
 
+let db = newDatabase()
+
 routes:
   get "/":
-    resp renderMain(renderLogin())
+    if request.cookies.hasKey("username"):
+      var user: User
+      if not db.findUser(request.cookies["username"], user):
+        user = User(username: request.cookies["username"], following: @[])
+        db.create(user)
+      let messages = db.findMessages(user.following & user.username)
+      resp renderMain(renderTimeline(user.username, messages))
+    else:
+      resp renderMain(renderLogin())
+
   post "/login":
     setCookie("username", @"username", getTime().getGMTime() + 2.hours)
+    redirect("/")
+
+  post "/createMessage":
+    let message = Message(username: @"username", time: getTime(), msg: @"message")
+    db.post(message)
     redirect("/")
 
 runForever()
